9f1bfa9cfb1b6a908f6386bf3e7a457d7b9d3700
etroshkova <Ekaterina.Troshkova@noveogroup.com>
2021-04-21 10:33:16 +0700

    Add splash screen

---
+++ app/src/main/java/nstu/avt716/etroshkova/diplom/presentation/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package nstu.avt716.etroshkova.diplom.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import nstu.avt716.etroshkova.diplom.presentation.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
